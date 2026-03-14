e680efad8ee8b1387a4fecc8dc8ee7e6e32b649e
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-03-14 13:24:54 +0530

    Update db_client.py

--- database/db_client.py
+++ database/db_client.py
@@ -1,10 +1,11 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URI
 
+# Initialize MongoDB Client
 client = AsyncIOMotorClient(MONGO_URI)
-db = client.id_provider_bot
+db = client.id_provider_db
 
 # Collections
-users_db = db.users
-accounts_db = db.accounts
-recharge_db = db.recharges
+users_db = db.users          # Stores user ID, balance, etc.
+accounts_db = db.accounts    # Stores available Telegram IDs & sessions
+recharges_db = db.recharges  # Stores pending/approved payment screenshots
